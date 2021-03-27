1aebec4bda337bc6985539d6aa39ee8076b81a99
Abdulrahman Beltagy <abdulrahman.abdeljawad@gmail.com>
2021-03-27 23:45:03 +0200

    migration

---
+++ MyCore/MyCore.MVC/Data/Migrations/20210324230441_intial_migration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyCore.MVC.Data.Migrations
+{
+    public partial class intial_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
